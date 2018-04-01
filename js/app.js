df75ae4b02110f9dfe5bb0d9c49f01a6283ac85f
Aleksandra <pyzakot@gmail.com>
2018-04-01 23:20:49 +0200

    Array of multiplied mixed cards added to gameboard

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+    var gameContainer = document.querySelector(".game-container");
+    var arrayOfCategories =  ["blue", "green", "pink", "purple", "purplelight"];
+    var shuffledArray;
+    var coveredCards;
+
+    // Multiplying elements in array in order to get pair of cards
+
+    function createArrayOfCardsPair(array) {
+        var gameArrayOfPairsOfCard = [];
+
+        for (var i = 0; i < 2; i++) {
+            for (var category in array) {
+                gameArrayOfPairsOfCard.push(array[category]);
+            }
+        }
+        return gameArrayOfPairsOfCard;
+    }
+    
+    // Reaordering array elements
+
+    function shuffle(array) {
+        var arrayOfCardsInRandomOrder = array.sort(function (a, b) {
+            return 0.5 - Math.random();
+        });
+
+        return arrayOfCardsInRandomOrder;
+    }
+
 
 });
