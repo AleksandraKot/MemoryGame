3ad1219e0a5bbb4dc54c2c0ca1a533f090d337c2
Aleksandra <pyzakot@gmail.com>
2018-04-02 00:17:51 +0200

    Restart game button centered. Event flipping cards on click added

--- js/app.js
+++ js/app.js
@@ -39,7 +39,93 @@ document.addEventListener("DOMContentLoaded", function () {
             gameContainer.appendChild(card);
         }
     }
-    createGameBoardOfFlippedCards(shuffle(createArrayOfCardsPair(arrayOfCategories)));
 
+    // Turning cards to front on click and show the picture
+
+    function turnCardsToFront(array) {
+        var counter = 0;
+
+        for (var i = 0; i < array.length; i++) {
+
+            array[i].addEventListener("click", function (e) {
+
+                var alreadyFlipedCards = document.querySelectorAll(".flip-in-hor-bottom");
+
+                if (alreadyFlipedCards.length < 2) {
+                    this.classList.toggle("flip-in-hor-bottom");
+                    this.classList.toggle("covered");
+                    this.classList.add(this.dataset.category);
+                }
+
+                alreadyFlipedCards = document.querySelectorAll(".flip-in-hor-bottom");
+                if (alreadyFlipedCards.length === 2) {
+                    hideIfTheSamePicture(alreadyFlipedCards);
+                    continueIfDifferent(alreadyFlipedCards);
+                }
+            });
+        }
+    }
+
+    function hideIfTheSamePicture(array) {
+        if (array[0].dataset.category === array[1].dataset.category) {
+            var timeout1 = setTimeout(function () {
+                console.log("te same obrazki");
+                array[0].classList.add("hidden");
+                array[1].classList.add("hidden");
+                array[0].classList.remove("flip-in-hor-bottom");
+                array[1].classList.remove("flip-in-hor-bottom");
+                addRestartButtonIfFinished();
+            }, 500);
+        }
+    }
+
+    function continueIfDifferent(array) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[0].dataset.category !== array[1].dataset.category) {
+
+                var timeout1 = setTimeout(function() {
+                    console.log("różne obrazki");
+                    array[0].classList.toggle("flip-in-hor-bottom");
+                    array[0].classList.add("covered");
+                    array[1].classList.toggle("flip-in-hor-bottom");
+                    array[1].classList.add("covered");
+                    array[0].classList.remove("flip-in-hor-bottom");
+                    array[1].classList.remove("flip-in-hor-bottom");
+                }, 500);
+            }
+        }
+    }
+    function addRestartButtonIfFinished() {
+        var hiddenCards = gameContainer.getElementsByClassName("hidden");
+        console.log(hiddenCards);
+
+        if (hiddenCards.length === 10) {
+            console.log("mam już 10 niewidocznych");
+            var restartButton = document.createElement("div");
+            restartButton.className = "restart-button";
+            restartButton.innerHTML = "Graj jeszcze raz";
+            gameContainer.appendChild(restartButton);
+            restartGame();
+        }
+    }
+
+    function restartGame() {
+        var restartButton = document.querySelector(".restart-button");
+
+        restartButton.addEventListener("click", function(e) {
+            restartButton.style.display = "none";
+            gameContainer.innerHTML = "";
+            startGame();
+        });
+    }
+
+    function startGame() {
+        shuffledArray = shuffle(createArrayOfCardsPair(arrayOfCategories));
+        createGameBoardOfFlippedCards(shuffledArray);
+        coveredCards = document.querySelectorAll(".covered");
+        turnCardsToFront(coveredCards);
+    }
+
+    startGame();
 
 });
